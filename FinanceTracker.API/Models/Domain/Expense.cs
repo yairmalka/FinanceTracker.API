4ec106fcec7e5d3993eb5a92a643274780750a63
Yair Malka <yairmalka9@gmail.com>
2024-04-14 23:48:33 +0300

    creating Expense and User models

--- FinanceTracker.API/Models/Domain/Expense.cs
+++ FinanceTracker.API/Models/Domain/Expense.cs
@@ -2,6 +2,14 @@
 {
     public class Expense
     {
-        
+        public Guid Id { get; set; }
+        public DateTime DateOfExpense { get; set; }
+        public decimal Amount {  get; set; }
+        public string Category { get; set; }
+        //public User UserID { get; set; } FIX IT: in the future after creating the user class
+        public string ReceiptImageUrl { get; set; }
+        public string Currency {  get; set; }
+        public string Location { get; set; }   
+
     }
 }
---
+++ FinanceTracker.API/Models/Domain/User.cs
@@ -0,0 +1,17 @@
+﻿namespace FinanceTracker.API.Models.Domain
+{
+    public class User
+    {
+
+        public class User
+        {
+            public Guid Id { get; set; }
+            public string Username { get; set; }
+            public string PasswordHash { get; set; }
+            public string Role { get; set; }
+            public string Email { get; set; }
+            public string FullName { get; set; }
+            public DateTime DateOfBirth { get; set; }
+        }
+    }
+}
